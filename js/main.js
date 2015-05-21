6912bbc14318489692f25f07fe0f55a95d36fb21
Aaron Washburn <aaronwashburn@Aarons-MacBook-Pro.local>
2015-05-21 09:13:59 -0400

    Add card moving, detect selected card, autoset top css

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
     var CARD_WIDTH = 73;
     var CARD_HEIGHT = 97;
+    var $CARD_OFFSET = 25;
+
     var SUITS = ['clubs', 'spades', 'hearts', 'diamonds'];
 
-
-    function placeCard( card_el, position_el ) {
-        position_el.append( card_el );
+    function deselectCard( card_el ) {
+        $(card_el).removeClass('selected');
     }
 
     function isTopCard( card_el ) {
@@ -16,6 +17,32 @@ $(document).ready(function() {
         return $(card_el).hasClass('selected');
     }
 
+    function moveCard( $card_el, $destination_el ) {
+        console.log('move card', $card_el, $destination_el );
+        $card_el.css('top','0');
+        $destination_el.append( $card_el );
+    }
+
+    function selectCard( card_el ) {
+        $(card_el).addClass('selected');
+    }
+
+    function cardSelected() {
+        var selected_ary = $('.card.selected');
+        return selected_ary[0]
+    }
+
+    function setCSSTop( $card_el ) {
+        var children = $card_el.parent().children();
+        var child_number = children.length;
+        var top = (child_number * $CARD_OFFSET) + 'px';
+        $card_el.css('top' , top)
+    }
+
+    function placeCard( card_el, position_el ) {
+        position_el.append( card_el );
+    }
+
     function Game() {
         var deck = this.deck;
         deck = new Deck();
@@ -23,17 +50,14 @@ $(document).ready(function() {
 
         this.deal = function() {
             var column = 1;
-            var top = 0;
             while ( deck.cards.length > 0 ) {
                 var $card_el = $(deck.cards.pop().el);
                 var top_string = ( top + 'px' );
-                console.log( $card_el );
-                $card_el.css('top', top_string );
                 $('.tableau .column.'+column).append( $card_el );
+                setCSSTop( $card_el );
                 column += 1;
                 if ( column > 8 ) {
                     column = 1;
-                    top += 25;
                 }
             }
         }
@@ -49,11 +73,14 @@ $(document).ready(function() {
 
     // card click handler
     $('.card').click( function() {
+        console.log( cardSelected() );
+
         if ( isTopCard( this ) ) {
             if ( isSelected( this ) ) {
-                $(this).removeClass('selected');
+                deselectCard( this );
             } else {
-                $(this).addClass('selected');
+                // moveCard( $(this), $('.foundations .column.1') );
+                selectCard( this );
             };
         };
     });
