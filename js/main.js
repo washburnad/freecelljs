62d56d5523efe6b8d9c19b3c92be0904b9fdebeb
Aaron Washburn <aaronwashburn@Aarons-MacBook-Pro.local>
2015-05-26 09:36:08 -0400

    Implement basic rules

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
     var $CARD_OFFSET = 25;
     var SUITS = ['clubs', 'spades', 'hearts', 'diamonds'];
 
+    // returns the card element with class .selected
     function cardSelected() {
         var selected_ary = $('.card.selected');
         return selected_ary[0]
@@ -13,7 +14,9 @@ $(document).ready(function() {
         $card_el.removeClass('selected');
     }
 
-    function isEmpty( $column_el ) {
+    // determines if the column element is in is empty
+    function isEmpty( $el ) {
+        var $column_el = $el.closest('.column');
         return ( $column_el.children().length == 0 )
     }
 
@@ -25,23 +28,86 @@ $(document).ready(function() {
         return $card_el.is(':last-child');
     }
 
+    function isValidMove( $card_el, $destination_el ) {
+        var dest_type = columnType( $destination_el )
+        console.log( dest_type );
+        switch (dest_type) {
+            case 'foundation':
+                return isValidFoundationMove( $card_el, $destination_el )
+            break;
+
+            case 'tableau':
+                return isValidTableauMove( $card_el, $destination_el )
+            break;
+
+            case 'freecell':
+                if ( isEmpty( $destination_el ) ) {
+                    return true;
+                } else {
+                    return false;
+                };
+            break;
+
+            default:
+                return false;
+        }
+    }
+
+    function isValidTableauMove( $card_el, $destination_el ) {
+        $rank = $card_el[0].rank;
+        $color = $card_el[0].color;
+        console.log($card_el[0], $rank, $color);
+        
+        if ( isEmpty( $destination_el ) ) {
+            return true;
+        } else {
+            $dest_rank = $destination_el[0].rank;
+            $dest_color = $destination_el[0].color;
+            console.log($destination_el[0], $dest_color, $dest_rank);
+        }
+        
+        if ( ( $color != $dest_color ) && ( $rank == $dest_rank - 1 ) ) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    function isValidFoundationMove( $card_el, $destination_el ) {
+        $rank = $card_el[0].rank;
+        $suit = $card_el[0].suit;
+        if ( isEmpty( $destination_el ) ) {
+            $dest_suit = $suit;
+            $dest_rank = 0;
+        } else {
+            $dest_rank = $destination_el[0].rank;
+            $dest_suit = $destination_el[0].suit;
+        }
+
+        if ( ( $suit == $dest_suit ) && ( $rank == $dest_rank + 1 ) ) {
+            return true
+        } else {
+            return false
+        }
+    }
+
     function moveCard( $card_el, $destination_el ) {
-        console.log('move card', $card_el, $destination_el );
-        deselectCard( $card_el );
-        placeCard( $card_el, $destination_el.closest('.column') );
-        // $destination_el.closest('.column').append( $card_el );
-        setCSSTop( $card_el );
+        if ( isValidMove( $card_el, $destination_el ) ) {
+            deselectCard( $card_el );
+            placeCard( $card_el, $destination_el.closest('.column') );
+            setCSSTop( $card_el );
+        };
     }
 
     function selectCard( $card_el ) {
         $card_el.addClass('selected');
     }
 
+    // determines the correct css 'top' value for cards placed in tableau
     function setCSSTop( $card_el ) {
         var $parent_el = $card_el.parent();
-        console.log($parent_el);
         var top = 0;
-        if ( $parent_el.data('column-type') == 'tableau' ) {
+        if ( columnType( $card_el ) == 'tableau' ) {
             var children = $parent_el.children();
             var child_number = children.length;
             top = ( ( child_number - 1 ) * $CARD_OFFSET) + 'px';
@@ -53,8 +119,8 @@ $(document).ready(function() {
         $position_el.append( $card_el );
     }
 
-    function isValidMove( $card_el, $destination_el ) {
-        
+    function columnType( $el ) {
+        return $el.closest('.column').data('column-type');
     }
 
     function Game() {
@@ -88,7 +154,6 @@ $(document).ready(function() {
     // card click handler
     $('.card').click( function() {
         var $this = $(this);
-        console.log( cardSelected() );
         var cardAlreadySelected = cardSelected();
         if ( isTopCard( $this ) ) {
             if ( isSelected( $this ) ) {
